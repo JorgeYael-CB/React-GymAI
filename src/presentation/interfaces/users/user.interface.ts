32bf544ab212645497f49968e3a0934df7e40f04
JorgeYael-CB <jorge.yael333@gmail.com>
2024-06-20 10:40:41 -0600

    Iniciando con las páginas de auth

--- src/presentation/interfaces/users/user.interface.ts
+++ src/presentation/interfaces/users/user.interface.ts
@@ -2,7 +2,6 @@ export type roles = 'USER' | 'SUPER_USER_BASIC' | 'SUPER_USER_PREMIUM' | 'SUPER_
 
 
 export interface UserDbInterface {
-
   id:string | number;
   name: string;
   email: string;
@@ -11,4 +10,6 @@ export interface UserDbInterface {
   verify: boolean;
   isActive: boolean;
   totalAmountPaid: number;
+  messages: any,
+  coments: any,
 }
