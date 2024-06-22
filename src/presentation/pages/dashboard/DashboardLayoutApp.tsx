b9d9ff2aa23b7a6eb1a3b58d1e8d41735dc1d361
JorgeYael-CB <jorge.yael333@gmail.com>
2024-06-22 17:36:59 -0600

    Agregamos el título del dashboard como un enlace

--- src/presentation/pages/dashboard/DashboardLayoutApp.tsx
+++ src/presentation/pages/dashboard/DashboardLayoutApp.tsx
@@ -1,14 +1,17 @@
-import { Outlet } from "react-router-dom";
+import {  Outlet, useNavigate } from "react-router-dom";
 import { menuDashboard } from "../../router/Router";
 import { SidebarItem } from "./SideBarItem";
 import './Dashboard.css';
 
 
 export const DashboardLayout = () => {
+  const nav = useNavigate();
+
+
   return (
     <main className="flex flex-row mt-7 dashboard">
       <nav className="hidden sm:flex flex-col ml-5 w-[370px] min-h-[calc(100vh-3.0rem)] bg-white bg-opacity-10 p-5 rounded-3xl">
-        <h1 className="font-bold text-lg lg:text-3xl bg-gradient-to-br from-white via-white/50 bg-clip-text text-transparent">
+        <h1 onClick={ () => nav('/') } className="font-bold text-lg lg:text-3xl bg-gradient-to-br from-white via-white/50 bg-clip-text text-transparent hover:cursor-pointer">
           Sport AI<span className="text-indigo-500">.</span>
         </h1>
         <span className="text-xl text-white mt-4">Bienvenido</span>
