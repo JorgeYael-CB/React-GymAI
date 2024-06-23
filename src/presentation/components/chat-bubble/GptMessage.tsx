e6cc244db86bb48c76936103279b245c23536f4a
JorgeYael-CB <jorge.yael333@gmail.com>
2024-06-22 22:13:20 -0600

    Agregamos el markdown en gpt

--- src/presentation/components/chat-bubble/GptMessage.tsx
+++ src/presentation/components/chat-bubble/GptMessage.tsx
@@ -4,22 +4,19 @@ interface Props {
   text: string;
 }
 
-
-
-export const GptMessage = ( {text}: Props ) => {
+export const GptMessage = ({ text }: Props) => {
   return (
     <div className="col-start-1 col-end-8 lg:p-3 pr-20 py-2 rounded-lg">
       <div className="flex flex-row items-start">
         <div className="flex items-center justify-center h-10 w-10 rounded-full bg-green-600 flex-shrink-0 max-lg:hidden">
           S
         </div>
-
         <div className="relative md:ml-3 font-medium text-xs md:text-sm bg-black bg-opacity-25 pt-3 pb-2 px-4 shadow rounded-xl">
           <Markdown>
-            { text }
+            {text}
           </Markdown>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
